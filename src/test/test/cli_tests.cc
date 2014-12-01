fa12188837b9571bfc193f1e084dd03e1a9b1c17
Brig Bagley <brig@sci.utah.edu>
2014-12-01 16:46:07 -0700

    removed wildcards, added scaling test

--- src/test/test/cli_tests.cc
+++ src/test/test/cli_tests.cc
@@ -53,30 +53,75 @@ static std::string command = std::string(BINARY_DIR) + _CLI + " -v ";
 static std::string name = _NAME + std::string("output");
 static std::string path = _PATH + data_dir;
 static std::string input = _FIELDS + data_dir + "input/" +_FILES;
-static const int num_files = 8;
-static std::string files[num_files] = {"background.node", "background.ele",
-  "sizing_field.nrrd", "boundary_field.nrrd", "boundary.nrrd",
-  "feature_field.nrrd", "medial.nrrd", "output.info"};
+static std::vector<std::string> files;
+
 // Tests basic IO for CLI
 TEST(CLIRegressionTests, Basic) {
   //make sure there is a command interpreter
-  ASSERT_TRUE(0!=(std::system(NULL)));
+  ASSERT_EQ(0,!(std::system(NULL)));
+  //add the common files to the list
+  files.push_back("background.node");
+  files.push_back("background.ele");
+  files.push_back("sizing_field.nrrd");
+  files.push_back("boundary_field.nrrd");
+  files.push_back("boundary.nrrd");
+  files.push_back("feature_field.nrrd");
+  files.push_back("medial.nrrd");
+  files.push_back("output.info");
   //setup the line that calls the command line interface
-  std::string output = " > " + data_dir + "basic_output.txt 2>&1";
+  files.push_back("output.node");
+  files.push_back("output.ele");
+  std::string log = "basic_output.txt";
+  files.push_back(log);
+  std::string output = " > " + data_dir + log + " 2>&1";
   std::string line = (command + name + path + input + output);
   //make sure there was no error from the command line
   ASSERT_EQ(0, std::system(line.c_str()));
   //move the other generated files in the current dir to the test dir
-  std::system(("mv background* *.nrrd " + data_dir).c_str());
+  for(size_t i = 0; i < 7; i++) {
+    std::system(("mv " + files[i] + " " + data_dir).c_str());
+  }
   //compare all of the files
   std::string diff(_DIFF);
-  for(size_t i = 0; i < num_files; i++) {
+  for(size_t i = 0; i < files.size() - 1; i++) {
     ASSERT_EQ(0, std::system((diff + data_dir +
             files[i] + " " + data_dir +
             "basic/" + files[i]).c_str()));
   }
   //delete the output files from this test
-  std::system(("rm " + data_dir + "*.nrrd " +
-        data_dir + "*.node " + data_dir + "*.txt " +
-        data_dir + "*.ele " + data_dir + "*.info").c_str());
+  for(size_t i = 0; i < files.size(); i++) {
+    std::system(("rm " + data_dir + files[i]).c_str());
+  }
+  //put the list of files back to the common ones
+  while (files.size() > 8) files.pop_back();
+}
+// Tests scaling IO for CLI
+TEST(CLIRegressionTests, Scaling) {
+  //setup the line that calls the command line interface
+  files.push_back("output.node");
+  files.push_back("output.ele");
+  std::string log = "scaling_output.txt";
+  files.push_back(log);
+  std::string output = " > " + data_dir + log + " 2>&1";
+  std::string scale = " --scale 1.0 ";
+  std::string line = (command + name + path + scale + input + output);
+  //make sure there was no error from the command line
+  ASSERT_EQ(0, std::system(line.c_str()));
+  //move the other generated files in the current dir to the test dir
+  for(size_t i = 0; i < 7; i++) {
+    std::system(("mv " + files[i] + " " + data_dir).c_str());
+  }
+  //compare all of the files
+  std::string diff(_DIFF);
+  for(size_t i = 0; i < files.size() - 1; i++) {
+    ASSERT_EQ(0, std::system((diff + data_dir +
+            files[i] + " " + data_dir +
+            "scaling/" + files[i]).c_str()));
+  }
+  //delete the output files from this test
+  for(size_t i = 0; i < files.size(); i++) {
+  //  std::system(("rm " + data_dir + files[i]).c_str());
+  }
+  //put the list of files back to the common ones
+  while (files.size() > 8) files.pop_back();
 }
