81bf5dcded6b5b3fa766c679d34b50115cd76857
Brig Bagley <brig@sci.utah.edu>
2014-12-02 12:48:39 -0700

    Windows works, but files differ with output details. Binary comparisons need to be replaced with file-reading float comparisons.

--- src/test/test/cli_tests.cc
+++ src/test/test/cli_tests.cc
@@ -46,14 +46,7 @@
 #define _NAME   " --output_name "
 #define _PATH   " --output_path "
 #define _CLI    "cleaver-cli "
-#define _DIFF   "cmake -E compare_files "
-#ifdef _WIN32
-#define _MV "move "
-#define _RM "del "
-#else
-#define _MV "mv "
-#define _RM "rm "
-#endif
+static std::string diff = std::string("diff -wB ");
 static std::string data_dir = std::string(TEST_DATA_DIR);
 static std::string command = std::string(BINARY_DIR) + _CLI + " -v ";
 static std::string name = _NAME + std::string("output");
@@ -89,10 +82,9 @@ TEST(CLIRegressionTests, Basic) {
   ASSERT_EQ(0, std::system(line.c_str()));
   //move the other generated files in the current dir to the test dir
   for(size_t i = 0; i < 7; i++) {
-    std::system((_MV + files[i] + " " + data_dir).c_str());
+    std::system(("mv " + files[i] + " " + data_dir).c_str());
   }
   //compare all of the files
-  std::string diff(_DIFF);
   for(size_t i = 0; i < files.size() - 1; i++) {
     ASSERT_EQ(0, std::system((diff + data_dir +
             files[i] + " " + data_dir +
@@ -100,7 +92,7 @@ TEST(CLIRegressionTests, Basic) {
   }
   //delete the output files from this test
   for(size_t i = 0; i < files.size(); i++) {
-    std::system((_RM + data_dir + files[i]).c_str());
+    std::system(("rm " + data_dir + files[i]).c_str());
   }
   //put the list of files back to the common ones
   while (files.size() > 8) files.pop_back();
@@ -119,10 +111,9 @@ TEST(CLIRegressionTests, Scaling) {
   ASSERT_EQ(0, std::system(line.c_str()));
   //move the other generated files in the current dir to the test dir
   for(size_t i = 0; i < 7; i++) {
-    std::system((_MV + files[i] + " " + data_dir).c_str());
+    std::system(("mv " + files[i] + " " + data_dir).c_str());
   }
   //compare all of the files
-  std::string diff(_DIFF);
   for(size_t i = 0; i < files.size() - 1; i++) {
     ASSERT_EQ(0, std::system((diff + data_dir +
             files[i] + " " + data_dir +
@@ -130,7 +121,7 @@ TEST(CLIRegressionTests, Scaling) {
   }
   //delete the output files from this test
   for(size_t i = 0; i < files.size(); i++) {
-    std::system((_RM + data_dir + files[i]).c_str());
+    std::system(("rm " + data_dir + files[i]).c_str());
   }
   //put the list of files back to the common ones
   while (files.size() > 8) files.pop_back();
