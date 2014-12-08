6c399bf90ece7e61e53a11c0d14479eb13b77cff
Brig Bagley <brig@sci.utah.edu>
2014-12-08 13:35:35 -0700

    File comparisons done by values, not file diffs.

--- src/test/test/cli_tests.cc
+++ src/test/test/cli_tests.cc
@@ -42,6 +42,7 @@
 #include <cstdlib>
 #include <cstdio>
 #include <fstream>
+#include <cmath>
 #define _FIELDS " --material_fields "
 #define _NAME   " --output_name "
 #define _PATH   " --output_path "
@@ -56,73 +57,141 @@ data_dir + "input/spheres1.nrrd " +
 data_dir + "input/spheres2.nrrd " +
 data_dir + "input/spheres3.nrrd " +
 data_dir + "input/spheres4.nrrd " ;
-static std::vector<std::string> files;
+static const size_t num_files = 7;
+static std::string files[num_files] = {
+  "background.node",
+  "background.ele",
+  "sizing_field.nrrd",
+  "boundary_field.nrrd",
+  "boundary.nrrd",
+  "feature_field.nrrd",
+  "medial.nrrd"};
+
+void compareEleFiles(const std::string a, const std::string b) {
+  ASSERT_FALSE(a == b);
+  std::ifstream test(b.c_str(),std::ifstream::in),
+    ans(a.c_str(),std::ifstream::in);
+  size_t count0, dim0, att0;
+  size_t count1, dim1, att1;
+  char line[512];
+  test.getline(line,512);
+  ans.getline(line,512);
+  test >> count0 >> dim0 >> att0;
+  ans  >> count1 >> dim1 >> att1;
+  ASSERT_EQ(count0,count1);
+  ASSERT_EQ(dim0,dim1);
+  ASSERT_EQ(att0,att1);
+  test.getline(line,512);
+  ans.getline(line,512);
+  for (size_t i = 0; i < count0; i++) {
+    size_t tmp = 0, tmp2 = 0;
+    size_t num0 = 0, num1 = 0;
+    test >> num0; ans >> num1;
+    ASSERT_EQ(num0,num1);
+    for(size_t j = 0; j < dim0; j++) {
+      test >> tmp; ans >> tmp2;
+      ASSERT_EQ(tmp,tmp2);
+    }
+    for(size_t j = 0; j < att0; j++) {
+      test >> tmp; ans >> tmp2;
+      ASSERT_EQ(tmp,tmp2);
+    }
+  }
+}
+
+void compareNodeFiles(const std::string a, const std::string b) {
+  ASSERT_FALSE(a == b);
+  std::ifstream test(b.c_str(),std::ifstream::in),
+    ans(a.c_str(),std::ifstream::in);
+  size_t count0, dim0, att0, bnd0;
+  size_t count1, dim1, att1, bnd1;
+  char line[512];
+  test.getline(line,512);
+  ans.getline(line,512);
+  test >> count0 >> dim0 >> att0 >> bnd0;
+  ans  >> count1 >> dim1 >> att1 >> bnd1;
+  ASSERT_EQ(count0,count1);
+  ASSERT_EQ(dim0,dim1);
+  ASSERT_EQ(att0,att1);
+  ASSERT_EQ(bnd0,bnd1);
+  test.getline(line,512);
+  ans.getline(line,512);
+  for (size_t i = 0; i < count0; i++) {
+    float tmp = 0.f, tmp2 = 0.f;
+    size_t num0 = 0, num1 = 0;
+    test >> num0; ans >> num1;
+    ASSERT_EQ(num0,num1);
+    for(size_t j = 0; j < dim0; j++) {
+      test >> tmp; ans >> tmp2;
+      ASSERT_FLOAT_EQ(tmp,tmp2);
+    }
+    for(size_t j = 0; j < att0; j++) {
+      test >> tmp; ans >> tmp2;
+      ASSERT_FLOAT_EQ(tmp,tmp2);
+    }
+    for(size_t j = 0; j < bnd0; j++) {
+      test >> tmp; ans >> tmp2;
+      ASSERT_FLOAT_EQ(tmp,tmp2);
+    }
+  }
+}
 
 // Tests basic IO for CLI
 TEST(CLIRegressionTests, Basic) {
   //make sure there is a command interpreter
   ASSERT_EQ(0,(int)!(std::system(NULL)));
-  //add the common files to the list
-  files.push_back("background.node");
-  files.push_back("background.ele");
-  files.push_back("sizing_field.nrrd");
-  files.push_back("boundary_field.nrrd");
-  files.push_back("boundary.nrrd");
-  files.push_back("feature_field.nrrd");
-  files.push_back("medial.nrrd");
-  files.push_back("output.info");
   //setup the line that calls the command line interface
-  files.push_back("output.node");
-  files.push_back("output.ele");
   std::string log = "basic_output.txt";
-  files.push_back(log);
   std::string output = " > " + data_dir + log + " 2>&1";
   std::string line = (command + name + path + input + output);
   //make sure there was no error from the command line
   ASSERT_EQ(0, std::system(line.c_str()));
   //move the other generated files in the current dir to the test dir
-  for(size_t i = 0; i < 7; i++) {
+  for(size_t i = 0; i < num_files; i++) {
     std::system(("mv " + files[i] + " " + data_dir).c_str());
   }
-  //compare all of the files
-  for(size_t i = 0; i < files.size() - 1; i++) {
-    ASSERT_EQ(0, std::system((diff + data_dir +
-            files[i] + " " + data_dir +
-            "basic/" + files[i]).c_str()));
-  }
+  //compare all of the related files
+  EXPECT_NO_FATAL_FAILURE(compareNodeFiles(
+        data_dir + "basic/output.node",
+        data_dir + "output.node"));
+  EXPECT_NO_FATAL_FAILURE(compareEleFiles(
+        data_dir + "basic/output.ele",
+        data_dir + "output.ele"));
   //delete the output files from this test
-  for(size_t i = 0; i < files.size(); i++) {
+  for(size_t i = 0; i < num_files; i++) {
     std::system(("rm " + data_dir + files[i]).c_str());
   }
-  //put the list of files back to the common ones
-  while (files.size() > 8) files.pop_back();
+  std::system(("rm " + data_dir + "output.info").c_str());
+  std::system(("rm " + data_dir + "output.node").c_str());
+  std::system(("rm " + data_dir + "output.ele").c_str());
+  std::system(("rm " + data_dir + log).c_str());
 }
 // Tests scaling IO for CLI
 TEST(CLIRegressionTests, Scaling) {
   //setup the line that calls the command line interface
-  files.push_back("output.node");
-  files.push_back("output.ele");
   std::string log = "scaling_output.txt";
-  files.push_back(log);
   std::string output = " > " + data_dir + log + " 2>&1";
   std::string scale = " --scale 0.5 ";
   std::string line = (command + name + path + scale + input + output);
   //make sure there was no error from the command line
   ASSERT_EQ(0, std::system(line.c_str()));
   //move the other generated files in the current dir to the test dir
-  for(size_t i = 0; i < 7; i++) {
+  for(size_t i = 0; i < num_files; i++) {
     std::system(("mv " + files[i] + " " + data_dir).c_str());
   }
-  //compare all of the files
-  for(size_t i = 0; i < files.size() - 1; i++) {
-    ASSERT_EQ(0, std::system((diff + data_dir +
-            files[i] + " " + data_dir +
-            "scaling/" + files[i]).c_str()));
-  }
+  //compare all of the related files
+  EXPECT_NO_FATAL_FAILURE(compareNodeFiles(
+        data_dir + "scaling/output.node",
+        data_dir + "output.node"));
+  EXPECT_NO_FATAL_FAILURE(compareEleFiles(
+        data_dir + "scaling/output.ele",
+        data_dir + "output.ele"));
   //delete the output files from this test
-  for(size_t i = 0; i < files.size(); i++) {
+  for(size_t i = 0; i < num_files; i++) {
     std::system(("rm " + data_dir + files[i]).c_str());
   }
-  //put the list of files back to the common ones
-  while (files.size() > 8) files.pop_back();
+  std::system(("rm " + data_dir + "output.info").c_str());
+  std::system(("rm " + data_dir + "output.node").c_str());
+  std::system(("rm " + data_dir + "output.ele").c_str());
+  std::system(("rm " + data_dir + log).c_str());
 }
