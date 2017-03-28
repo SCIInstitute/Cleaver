SCIInstitute/Cleaver

commit ab76493fa09ee2be75b5e2591010ea7800d2153d

Numerous improvements
 * Data and Mesh option widgets now sync with loads/runs
 * Options are unavailable when the current data is insufficient
 * Adaptive vs. Constant is working
 * Face / Cell locks work independently
 * Toggle on/off data widget view
 * Removed useless camera options
 * Only 1 sizing field, 1 volume, 1 mesh, and > 1 indicators allowed

src/lib/cleaver/CleaverMesher.h
@@ -2,18 +2,14 @@
 #define CLEAVERMESHER_H
 
 #include "Octree.h"
+#include "CleaverMesherImpl.h"
 
-namespace cleaver
-{
-    class Volume;
-    class TetMesh;
-    class CleaverMesherImp;
-
+namespace cleaver {
 class CleaverMesher
 {
 public:
 
-    CleaverMesher(const Volume *volume);
+    CleaverMesher();
     ~CleaverMesher();
 
     void createTetMesh(bool verbose);
@@ -26,9 +22,7 @@ public:
     void cleanup();    
     //================================
 
-    enum TopologyMode { TopologyModeNone, TopologyModeSubdivide, TopologyModeCleave };
-
-    void setTopologyMode(TopologyMode mode);
+    void setTopologyMode(CleaverMesherImp::TopologyMode mode);
 		void setAlphaInit(double alpha);
 
     //================================
@@ -78,8 +72,7 @@ public:
     void setRegular(bool reg);
 
 private:
-
-    CleaverMesherImp *m_pimpl;
+    CleaverMesherImp m_pimpl;
     double m_alpha_long;
     double m_alpha_short;
     bool m_regular;

src/lib/cleaver/CleaverMesherImpl.h
@@ -1,16 +1,19 @@
+#ifndef __CLEAVERMESHIMP_H__
+#define __CLEAVERMESHIMP_H__
+
 #include "vec3.h"
 #include "TetMesh.h"
+#include "Volume.h"
 #include "Vertex.h"
 #include "ScalarField.h"
 #include "SizingFieldOracle.h"
 #include <cmath>
 #include <cstdlib>
-#include "CleaverMesher.h"
 
 namespace cleaver {
 
-#ifndef NULL
-#define NULL 0
+#ifndef nullptr
+#define nullptr 0
 #endif
 
 #define VERT 0
@@ -68,6 +71,9 @@ private:
 class CleaverMesherImp
 {
 public:
+
+    enum TopologyMode { TopologyModeNone, TopologyModeSubdivide, TopologyModeCleave };
+
     CleaverMesherImp();
     ~CleaverMesherImp();
     ScalarField<float>* createSizingField();
@@ -181,7 +187,7 @@ public:
     double m_background_time;
     double m_cleaving_time;
 
-    CleaverMesher::TopologyMode m_topologyMode;
+    TopologyMode m_topologyMode;
     double m_alpha_init;
 
     Volume *m_volume;
@@ -195,3 +201,5 @@ public:
 };
 
 } // namespace cleaver
+
+#endif // !__CLEAVERMESHIMP_H__

src/cli/mesher/main.cpp
@@ -371,13 +371,14 @@ int main(int argc,  char* argv[])
       fields.push_back(new cleaver::InverseScalarField(fields[0]));
   }
   cleaver::Volume *volume = new cleaver::Volume(fields);
-  cleaver::CleaverMesher mesher(volume);
+  cleaver::CleaverMesher mesher;
+  mesher.setVolume(volume);
   mesher.setAlphaInit(alpha);
 
   //-----------------------------------
   // Load background mesh if provided
   //-----------------------------------
-  cleaver::TetMesh *bgMesh = NULL;
+  cleaver::TetMesh *bgMesh = nullptr;
   if(have_background_mesh) {
     std::string nodeFileName = background_mesh + ".node";
     std::string eleFileName = background_mesh + ".ele";

src/gui/Application/Data/DataManager.cpp
@@ -3,54 +3,25 @@
 #include <Cleaver/InverseField.h>
 #include <Cleaver/AbstractScalarField.h>
 
-DataManager::DataManager() : mesh_(NULL), sizingField_(NULL), volume_(NULL) {}
+DataManager::DataManager() : mesh_(nullptr), sizingField_(nullptr), volume_(nullptr) {}
 
 
-DataManager::~DataManager(){
-  if (this->mesh_ != NULL) {
-    delete this->mesh_;
-  }
-  if (this->sizingField_ != NULL) {
-    delete this->sizingField_;
-  }
-  for (auto a : this->indicators_) {
-    if (a != NULL) {
-      delete a;
-    }
-  }
-  if (this->volume_ != NULL) {
-    delete this->volume_;
-  }
-}
+DataManager::~DataManager(){}
 
 void DataManager::setMesh(cleaver::TetMesh *mesh) {
-  if (this->mesh_ != NULL) {
-    delete this->mesh_;
-  }
   this->mesh_ = mesh;
 }
 
 void DataManager::setSizingField(cleaver::AbstractScalarField *field) {
-  if (this->sizingField_ != NULL) {
-    delete this->sizingField_;
-  }
   this->sizingField_ = field;
   this->volume_->setSizingField(field);
 }
 
 void DataManager::setIndicators(std::vector<cleaver::AbstractScalarField *> indicators) {
-  for (auto a : this->indicators_) {
-    if (a != NULL) {
-      delete a;
-    }
-  }
   this->indicators_ = indicators;
 }
 
 void DataManager::setVolume(cleaver::Volume *volume) {
-  if (this->volume_ != NULL) {
-    delete this->volume_;
-  }
   this->volume_ = volume;
 }
 

src/gui/Application/DataWidgets/DataGroupWidget.cpp
@@ -1,14 +1,0 @@
-#include "DataGroupWidget.h"
-#include "ui_DataGroupWidget.h"
-
-DataGroupWidget::DataGroupWidget(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::DataGroupWidget)
-{
-    ui->setupUi(this);
-}
-
-DataGroupWidget::~DataGroupWidget()
-{
-    delete ui;
-}

src/gui/Application/DataWidgets/DataGroupWidget.h
@@ -1,22 +1,0 @@
-#ifndef DATAGROUPWIDGET_H
-#define DATAGROUPWIDGET_H
-
-#include <QWidget>
-
-namespace Ui {
-class DataGroupWidget;
-}
-
-class DataGroupWidget : public QWidget
-{
-    Q_OBJECT
-    
-public:
-    explicit DataGroupWidget(QWidget *parent = 0);
-    ~DataGroupWidget();
-    
-private:
-    Ui::DataGroupWidget *ui;
-};
-
-#endif // DATAGROUPWIDGET_H

src/gui/Application/DataWidgets/DataGroupWidget.ui
@@ -1,148 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>DataGroupWidget</class>
- <widget class="QWidget" name="DataGroupWidget">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>210</width>
-    <height>75</height>
-   </rect>
-  </property>
-  <property name="sizePolicy">
-   <sizepolicy hsizetype="Preferred" vsizetype="Fixed">
-    <horstretch>0</horstretch>
-    <verstretch>0</verstretch>
-   </sizepolicy>
-  </property>
-  <property name="windowTitle">
-   <string>Form</string>
-  </property>
-  <layout class="QVBoxLayout" name="verticalLayout">
-   <property name="spacing">
-    <number>0</number>
-   </property>
-   <property name="margin">
-    <number>0</number>
-   </property>
-   <item>
-    <widget class="QWidget" name="base" native="true">
-     <layout class="QVBoxLayout" name="verticalLayout_2">
-      <property name="spacing">
-       <number>0</number>
-      </property>
-      <property name="margin">
-       <number>1</number>
-      </property>
-      <item>
-       <widget class="QWidget" name="groupBackground" native="true">
-        <layout class="QHBoxLayout" name="horizontalLayout">
-         <property name="spacing">
-          <number>0</number>
-         </property>
-         <property name="leftMargin">
-          <number>0</number>
-         </property>
-         <property name="topMargin">
-          <number>0</number>
-         </property>
-         <property name="rightMargin">
-          <number>6</number>
-         </property>
-         <property name="bottomMargin">
-          <number>0</number>
-         </property>
-         <item>
-          <widget class="QWidget" name="groupHeader" native="true">
-           <property name="styleSheet">
-            <string notr="true"/>
-           </property>
-           <layout class="QHBoxLayout" name="horizontalLayout_2">
-            <item>
-             <widget class="QToolButton" name="toolButton">
-              <property name="minimumSize">
-               <size>
-                <width>21</width>
-                <height>21</height>
-               </size>
-              </property>
-              <property name="maximumSize">
-               <size>
-                <width>21</width>
-                <height>21</height>
-               </size>
-              </property>
-              <property name="text">
-               <string/>
-              </property>
-              <property name="icon">
-               <iconset resource="../Resources/QtResources.qrc">
-                <normaloff>:/icons/Icons/RightArrowWhite.png</normaloff>
-                <normalon>:/icons/Icons/DownArrowWhite.png</normalon>:/icons/Icons/RightArrowWhite.png</iconset>
-              </property>
-              <property name="checkable">
-               <bool>true</bool>
-              </property>
-              <property name="checked">
-               <bool>true</bool>
-              </property>
-             </widget>
-            </item>
-            <item>
-             <spacer name="horizontalSpacer">
-              <property name="orientation">
-               <enum>Qt::Horizontal</enum>
-              </property>
-              <property name="sizeHint" stdset="0">
-               <size>
-                <width>40</width>
-                <height>20</height>
-               </size>
-              </property>
-             </spacer>
-            </item>
-            <item>
-             <widget class="QLabel" name="label">
-              <property name="palette">
-               <palette>
-                <active/>
-                <inactive/>
-                <disabled/>
-               </palette>
-              </property>
-              <property name="text">
-               <string>TextLabel</string>
-              </property>
-             </widget>
-            </item>
-           </layout>
-          </widget>
-         </item>
-        </layout>
-       </widget>
-      </item>
-      <item>
-       <widget class="QLabel" name="fakeWidget">
-        <property name="text">
-         <string/>
-        </property>
-       </widget>
-      </item>
-     </layout>
-    </widget>
-   </item>
-   <item>
-    <widget class="QLabel" name="groupDummy">
-     <property name="text">
-      <string/>
-     </property>
-    </widget>
-   </item>
-  </layout>
- </widget>
- <resources>
-  <include location="../Resources/QtResources.qrc"/>
- </resources>
- <connections/>
-</ui>

commit 16709539cccc9369f323166e67e1ea6167604cdc

Fixes pimpl ideom that was broken by commit ab76493fa09ee2be75b5e2591010ea7800d2153d
This change fixes the pimpl idiom used in the CleaverMesher. The idiom hides the actual
interface from the front end to prevent accidental or intentional tight coupling of the
implementation details. Commit ab76493fa09ee2be75b5e2591010ea7800d2153d inverted this
idiom, exposing implementation details to the main interface and defeating the purpose
of the pattern.

src/lib/cleaver/CleaverMesher.cpp
@@ -1,4 +1,5 @@
 #include "CleaverMesher.h"
+#include "CleaverMesherImpl.h"
 #include "StencilTable.h"
 #include "TetMesh.h"
 #include "Octree.h"
@@ -141,22 +142,24 @@ namespace cleaver
   }
 
   // -- state getters --
-  bool CleaverMesher::backgroundMeshCreated() const { return m_pimpl.m_bBackgroundMeshCreated; }
-  bool CleaverMesher::adjacencyBuilt()        const { return m_pimpl.m_bAdjacencyBuilt; }
-  bool CleaverMesher::samplingDone()          const { return m_pimpl.m_bSamplingDone; }
-  bool CleaverMesher::alphasComputed()        const { return m_pimpl.m_bAlphasComputed; }
-  bool CleaverMesher::interfacesComputed()    const { return m_pimpl.m_bInterfacesComputed; }
-  bool CleaverMesher::generalized()           const { return m_pimpl.m_bGeneralized; }
-  bool CleaverMesher::snapsAndWarpsDone()     const { return m_pimpl.m_bSnapsAndWarpsDone; }
-  bool CleaverMesher::stencilsDone()          const { return m_pimpl.m_bStencilsDone; }
-  bool CleaverMesher::completed()             const { return m_pimpl.m_bComplete; }
+  bool CleaverMesher::backgroundMeshCreated() const { return m_pimpl->m_bBackgroundMeshCreated; }
+  bool CleaverMesher::adjacencyBuilt()        const { return m_pimpl->m_bAdjacencyBuilt; }
+  bool CleaverMesher::samplingDone()          const { return m_pimpl->m_bSamplingDone; }
+  bool CleaverMesher::alphasComputed()        const { return m_pimpl->m_bAlphasComputed; }
+  bool CleaverMesher::interfacesComputed()    const { return m_pimpl->m_bInterfacesComputed; }
+  bool CleaverMesher::generalized()           const { return m_pimpl->m_bGeneralized; }
+  bool CleaverMesher::snapsAndWarpsDone()     const { return m_pimpl->m_bSnapsAndWarpsDone; }
+  bool CleaverMesher::stencilsDone()          const { return m_pimpl->m_bStencilsDone; }
+  bool CleaverMesher::completed()             const { return m_pimpl->m_bComplete; }
 
 
   CleaverMesher::~CleaverMesher() {
-    cleanup();
+    // cleanup();
+    delete m_pimpl;
   }
 
-  CleaverMesher::CleaverMesher() {
+  CleaverMesher::CleaverMesher() : m_pimpl(new CleaverMesherImp)
+  {
     m_alpha_long = DEFAULT_ALPHA_LONG;
     m_alpha_short = DEFAULT_ALPHA_SHORT;
     m_regular = false;
@@ -164,59 +167,59 @@ namespace cleaver
 
   void CleaverMesher::createTetMesh(bool verbose)
   {
-    m_pimpl.createBackgroundMesh(verbose);
-    m_pimpl.buildAdjacency(verbose);
-    m_pimpl.sampleVolume(verbose);
-    m_pimpl.computeAlphas(verbose);
-    m_pimpl.computeInterfaces(verbose);
-    m_pimpl.generalizeTets(verbose);
-    m_pimpl.snapAndWarpViolations(verbose);
-    m_pimpl.stencilBackgroundTets(verbose);
+    m_pimpl->createBackgroundMesh(verbose);
+    m_pimpl->buildAdjacency(verbose);
+    m_pimpl->sampleVolume(verbose);
+    m_pimpl->computeAlphas(verbose);
+    m_pimpl->computeInterfaces(verbose);
+    m_pimpl->generalizeTets(verbose);
+    m_pimpl->snapAndWarpViolations(verbose);
+    m_pimpl->stencilBackgroundTets(verbose);
   }
 
+  // TODO(jonbronson): This method should not be necessary. Investigate cases
+  // where it is actually changing mesh and track down root cause.
   size_t CleaverMesher::fixVertexWindup(bool verbose) {
-    return m_pimpl.m_mesh->fixVertexWindup(verbose);
+    return m_pimpl->m_mesh->fixVertexWindup(verbose);
   }
 
   TetMesh* CleaverMesher::getBackgroundMesh() const
   {
-    return m_pimpl.m_bgMesh;
+    return m_pimpl->m_bgMesh;
   }
 
   TetMesh* CleaverMesher::getTetMesh() const
   {
-    return m_pimpl.m_mesh;
+    return m_pimpl->m_mesh;
   }
 
 
   Volume* CleaverMesher::getVolume() const
   {
-    return m_pimpl.m_volume;
+    return m_pimpl->m_volume;
   }
 
   void CleaverMesher::cleanup()
   {
-    if (m_pimpl.m_bgMesh)
-      delete m_pimpl.m_bgMesh;
-    m_pimpl.m_bgMesh = nullptr;
+    if (m_pimpl->m_bgMesh)
+      delete m_pimpl->m_bgMesh;
+    m_pimpl->m_bgMesh = nullptr;
   }
 
   void CleaverMesher::setVolume(const Volume *volume)
   {
     cleanup();
-    m_pimpl.m_volume = const_cast<Volume*>(volume);
-    m_pimpl.m_bgMesh = nullptr;
-    m_pimpl.m_mesh = nullptr;
+    m_pimpl->m_volume = const_cast<Volume*>(volume);
   }
 
-  void CleaverMesher::setTopologyMode(CleaverMesherImp::TopologyMode mode)
+  void CleaverMesher::setTopologyMode(TopologyMode mode)
   {
-    m_pimpl.m_topologyMode = mode;
+    m_pimpl->m_topologyMode = mode;
   }
 
   void CleaverMesher::setAlphaInit(double alpha)
   {
-    m_pimpl.m_alpha_init = alpha;
+    m_pimpl->m_alpha_init = alpha;
   }
 
   //============================================================================
@@ -1762,7 +1765,7 @@ namespace cleaver
   //==============================
   Octree* CleaverMesher::getTree() const
   {
-    return m_pimpl.m_tree;
+    return m_pimpl->m_tree;
   }
 
   //========================================
@@ -1772,7 +1775,7 @@ namespace cleaver
   {
     cleaver::Timer timer;
     timer.start();
-    TetMesh* m = m_pimpl.createBackgroundMesh(verbose);
+    TetMesh* m = m_pimpl->createBackgroundMesh(verbose);
     timer.stop();
     setBackgroundTime(timer.time());
     return m;
@@ -1780,7 +1783,7 @@ namespace cleaver
 
   void CleaverMesher::setBackgroundMesh(TetMesh *m)
   {
-    m_pimpl.setBackgroundMesh(m);
+    m_pimpl->setBackgroundMesh(m);
   }
 
   //==================================
@@ -1788,7 +1791,7 @@ namespace cleaver
   //==================================
   void CleaverMesher::buildAdjacency(bool verbose)
   {
-    m_pimpl.buildAdjacency(verbose);
+    m_pimpl->buildAdjacency(verbose);
   }
 
   //================================
@@ -1796,7 +1799,7 @@ namespace cleaver
   //================================
   void CleaverMesher::sampleVolume(bool verbose)
   {
-    m_pimpl.sampleVolume(verbose);
+    m_pimpl->sampleVolume(verbose);
   }
 
   //=================================
@@ -1804,7 +1807,7 @@ namespace cleaver
   //=================================
   void CleaverMesher::computeAlphas(bool verbose)
   {
-    m_pimpl.computeAlphas(verbose, m_regular, m_alpha_long, m_alpha_short);
+    m_pimpl->computeAlphas(verbose, m_regular, m_alpha_long, m_alpha_short);
   }
 
   //=====================================
@@ -1812,7 +1815,7 @@ namespace cleaver
   //=====================================
   void CleaverMesher::computeInterfaces(bool verbose)
   {
-    m_pimpl.computeInterfaces(verbose);
+    m_pimpl->computeInterfaces(verbose);
   }
 
   //=====================================
@@ -1820,7 +1823,7 @@ namespace cleaver
   //=====================================
   void CleaverMesher::generalizeTets(bool verbose)
   {
-    m_pimpl.generalizeTets(verbose);
+    m_pimpl->generalizeTets(verbose);
   }
 
   //================================
@@ -1828,7 +1831,7 @@ namespace cleaver
   //================================
   void CleaverMesher::snapsAndWarp(bool verbose)
   {
-    m_pimpl.snapAndWarpViolations(verbose);
+    m_pimpl->snapAndWarpViolations(verbose);
   }
 
   //===============================
@@ -1836,7 +1839,7 @@ namespace cleaver
   //===============================
   void CleaverMesher::stencilTets(bool verbose)
   {
-    m_pimpl.stencilBackgroundTets(verbose);
+    m_pimpl->stencilBackgroundTets(verbose);
   }
 
 
@@ -6911,32 +6914,32 @@ namespace cleaver
   //=================================================
   void CleaverMesher::setSizingFieldTime(double time)
   {
-    m_pimpl.m_sizing_field_time = time;
+    m_pimpl->m_sizing_field_time = time;
   }
 
   void CleaverMesher::setBackgroundTime(double time)
   {
-    m_pimpl.m_background_time = time;
+    m_pimpl->m_background_time = time;
   }
 
   void CleaverMesher::setCleavingTime(double time)
   {
-    m_pimpl.m_cleaving_time = time;
+    m_pimpl->m_cleaving_time = time;
   }
 
   double CleaverMesher::getSizingFieldTime() const
   {
-    return m_pimpl.m_sizing_field_time;
+    return m_pimpl->m_sizing_field_time;
   }
 
   double CleaverMesher::getBackgroundTime() const
   {
-    return m_pimpl.m_background_time;
+    return m_pimpl->m_background_time;
   }
 
   double CleaverMesher::getCleavingTime() const
   {
-    return m_pimpl.m_cleaving_time;
+    return m_pimpl->m_cleaving_time;
   }
 
 }

src/lib/cleaver/CleaverMesher.h
@@ -2,9 +2,13 @@
 #define CLEAVERMESHER_H
 
 #include "Octree.h"
-#include "CleaverMesherImpl.h"
 
 namespace cleaver {
+
+class Volume;
+class TetMesh;
+class CleaverMesherImp;
+
 class CleaverMesher
 {
 public:
@@ -22,7 +26,9 @@ public:
     void cleanup();
     //================================
 
-    void setTopologyMode(CleaverMesherImp::TopologyMode mode);
+    enum TopologyMode { TopologyModeNone, TopologyModeSubdivide, TopologyModeCleave };
+
+    void setTopologyMode(TopologyMode mode);
     void setAlphaInit(double alpha);
 
     //================================
@@ -72,7 +78,7 @@ public:
     void setRegular(bool reg);
 
 private:
-    CleaverMesherImp m_pimpl;
+    CleaverMesherImp *m_pimpl;
     double m_alpha_long;
     double m_alpha_short;
     bool m_regular;

src/lib/cleaver/CleaverMesherImpl.h
@@ -10,6 +10,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <cstdint>
+#include "CleaverMesher.h"
 
 namespace cleaver {
 
@@ -67,9 +68,6 @@ private:
 class CleaverMesherImp
 {
 public:
-
-    enum TopologyMode { TopologyModeNone, TopologyModeSubdivide, TopologyModeCleave };
-
     CleaverMesherImp();
     ~CleaverMesherImp();
     ScalarField<float>* createSizingField();
@@ -183,7 +181,7 @@ public:
     double m_background_time;
     double m_cleaving_time;
 
-    TopologyMode m_topologyMode;
+    CleaverMesher::TopologyMode m_topologyMode;
     double m_alpha_init;
 
     Volume *m_volume;