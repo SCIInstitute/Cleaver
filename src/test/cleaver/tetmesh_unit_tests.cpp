2b2cc836bda69d33c59dd5bcb0193efa3d7e1e02
Jonathan Bronson <jonbronson@gmail.com>
2017-07-28 16:08:31 -0600

    improve readability

--- src/test/cleaver/tetmesh_unit_tests.cpp
+++ src/test/cleaver/tetmesh_unit_tests.cpp
@@ -41,10 +41,12 @@
 #include "TetMesh.h"
 #include "gtest/gtest.h"
 #include <cmath>
+
 using namespace cleaver;
+
 // Tests min/max Angles of regular tet
 TEST(AngleTests, Regular) {
-  //regular 1
+  // Regular 1
   Vertex v1,v2,v3,v4;
   v1.pos() = vec3(0.f,0.f,sqrt(2.f/3.f) - .5f/sqrt(6.f));
   v2.pos() = vec3(-.5f/sqrt(3.f),-.5f,-.5f/sqrt(6.f));
@@ -53,7 +55,8 @@ TEST(AngleTests, Regular) {
   Tet tet(&v1,&v2,&v3,&v4,0);
   ASSERT_FLOAT_EQ(70.5288f, tet.minAngle());
   ASSERT_FLOAT_EQ(70.5288f, tet.maxAngle());
-  //regular 2
+
+  // Regular 2
   v1.pos() = vec3( 1.f, 0.f,-1.f/sqrt(2.f));
   v2.pos() = vec3(-1.f, 0.f,-1.f/sqrt(2.f));
   v3.pos() = vec3( 0.f, 1.f, 1.f/sqrt(2.f));
@@ -62,7 +65,8 @@ TEST(AngleTests, Regular) {
   ASSERT_FLOAT_EQ(70.5288f, tet2.minAngle());
   ASSERT_FLOAT_EQ(70.5288f, tet2.maxAngle());
 }
-//Tests a right angled tet
+
+// Tests a right angled tet.
 TEST(AngleTests,Right) {
   Vertex v1,v2,v3,v4;
   v1.pos() = vec3(0.f,0.f,1.f);
@@ -73,7 +77,8 @@ TEST(AngleTests,Right) {
   ASSERT_FLOAT_EQ(54.7356103f, tet.minAngle());
   ASSERT_FLOAT_EQ(90.0f, tet.maxAngle());
 }
-//Tests a flat tet
+
+// Tests a flat tet.
 TEST(AngleTests,Flat) {
   Vertex v1,v2,v3,v4;
   v1.pos() = vec3(0.25f,0.25f,.0f);
